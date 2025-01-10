97dfd7171bd33592fd9f81fe767fbad95aa581dd
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2025-01-10 14:38:02 +0700

    feat: add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "trace-creative-app",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      cwd: "./",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
