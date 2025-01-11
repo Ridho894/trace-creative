2c11be53c4160b8db6047c406c2e3544105a4c77
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2025-01-11 20:44:58 +0700

    feat: add products picture

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -45,107 +45,452 @@ const catalogItems = [
 ];
 
 const Clothing = [
-  { id: 1, name: "T-Shirt", image: "https://via.placeholder.com/150" },
-  { id: 2, name: "Jacket", image: "https://via.placeholder.com/150" },
+  {
+    id: 1,
+    name: "Baseball Varsity",
+    image: "/products/clothing/Baseball Varsity.png",
+  },
+  {
+    id: 2,
+    name: "Batik’s Shirt",
+    image: "/products/clothing/Batik’s Shirt.png",
+  },
+  {
+    id: 3,
+    name: "Blazer Men",
+    image: "/products/clothing/Blazer.png",
+  },
+  {
+    id: 4,
+    name: "Blazer Women",
+    image: "/products/clothing/Blazer(1).png",
+  },
+  {
+    id: 5,
+    name: "Bomber Jacket",
+    image: "/products/clothing/Bomber Jacket.png",
+  },
+  {
+    id: 6,
+    name: "Crewneck",
+    image: "/products/clothing/Crewneck.png",
+  },
+  {
+    id: 7,
+    name: "Full Print",
+    image: "/products/clothing/Full Print.png",
+  },
+  {
+    id: 8,
+    name: "Jersey",
+    image: "/products/clothing/Jersey.png",
+  },
+  {
+    id: 9,
+    name: "Long T-shirt",
+    image: "/products/clothing/Long T-shirt.png",
+  },
+  {
+    id: 10,
+    name: "Polo",
+    image: "/products/clothing/Polo.png",
+  },
+  {
+    id: 11,
+    name: "Raglan",
+    image: "/products/clothing/Raglan.png",
+  },
+  {
+    id: 12,
+    name: "Riding Jacket",
+    image: "/products/clothing/Riding Jacket.png",
+  },
+  {
+    id: 13,
+    name: "Shirt",
+    image: "/products/clothing/Shirt.png",
+  },
+  {
+    id: 14,
+    name: "Sweater Hoodie",
+    image: "/products/clothing/Sweater Hoodie.png",
+  },
+  {
+    id: 15,
+    name: "T-shirt",
+    image: "/products/clothing/T-shirt.png",
+  },
+  {
+    id: 16,
+    name: "Trouser",
+    image: "/products/clothing/Trouser.png",
+  },
+  {
+    id: 17,
+    name: "Turtleneck Zipper",
+    image: "/products/clothing/Turtleneck Zipper.png",
+  },
+  {
+    id: 18,
+    name: "Utility Jacket",
+    image: "/products/clothing/Utility Jacket.png",
+  },
+  {
+    id: 19,
+    name: "Vest",
+    image: "/products/clothing/Vest.png",
+  },
+  {
+    id: 20,
+    name: "Zipper Hoodie",
+    image: "/products/clothing/Zipper Hoodie.png",
+  },
 ];
 
 const Accessories = [
-  { id: 3, name: "Necklace", image: "https://via.placeholder.com/150" },
-  { id: 4, name: "Watch", image: "https://via.placeholder.com/150" },
+  { id: 1, name: "Cap", image: "/products/accessories/Cap.png" },
+  { id: 2, name: "Clock", image: "/products/accessories/Clock.png" },
+  { id: 3, name: "Golf Ball", image: "/products/accessories/Golf Ball.png" },
+  {
+    id: 4,
+    name: "Luggage Case",
+    image: "/products/accessories/Luggage Case.png",
+  },
+  { id: 5, name: "Pin", image: "/products/accessories/Pin.png" },
+  { id: 6, name: "Towel", image: "/products/accessories/Towel.png" },
+  { id: 7, name: "Umbrella", image: "/products/accessories/Umbrella.png" },
 ];
 
 const Drinkware = [
-  { id: 5, name: "Mug", image: "https://via.placeholder.com/150" },
-  { id: 6, name: "Water Bottle", image: "https://via.placeholder.com/150" },
+  { id: 1, name: "Corkcicle", image: "/products/drinkware/Corkcicle.png" },
+  { id: 2, name: "Hydro Flask", image: "/products/drinkware/Hydro Flask.png" },
+  {
+    id: 3,
+    name: "Infused Bottle",
+    image: "/products/drinkware/Infused Bottle.png",
+  },
+  { id: 4, name: "LED Bottle", image: "/products/drinkware/LED Bottle.png" },
+  { id: 5, name: "Lock & Lock", image: "/products/drinkware/Lock & Lock.png" },
+  { id: 6, name: "Mugs", image: "/products/drinkware/Mugs.png" },
+  {
+    id: 7,
+    name: "Stainless Bottle",
+    image: "/products/drinkware/Stainless Bottle.png",
+  },
+  { id: 8, name: "Stanley", image: "/products/drinkware/Stanley.png" },
+  {
+    id: 9,
+    name: "Water Bottle",
+    image: "/products/drinkware/Water Bottle.png",
+  },
 ];
 
 const BagsAndWallets = [
-  { id: 9, name: "Notebook", image: "https://via.placeholder.com/150" },
-  { id: 10, name: "Pen", image: "https://via.placeholder.com/150" },
+  { id: 1, name: "Backpack", image: "/products/bags-and-wallets/Backpack.png" },
+  {
+    id: 2,
+    name: "Card Holder",
+    image: "/products/bags-and-wallets/Card Holder.png",
+  },
+  { id: 3, name: "Clutch", image: "/products/bags-and-wallets/Clutch.png" },
+  {
+    id: 4,
+    name: "Drawstring Bag",
+    image: "/products/bags-and-wallets/Drawstring Bag.png",
+  },
+  {
+    id: 5,
+    name: "Duffle Bag",
+    image: "/products/bags-and-wallets/Duffle Bag.png",
+  },
+  {
+    id: 6,
+    name: "Laptop Bag",
+    image: "/products/bags-and-wallets/Laptop Bag.png",
+  },
+  { id: 7, name: "Luggage", image: "/products/bags-and-wallets/Luggage.png" },
+  {
+    id: 8,
+    name: "Pouch",
+    image: "/products/bags-and-wallets/Pouch.png",
+  },
+  {
+    id: 9,
+    name: "PVC Bag",
+    image: "/products/bags-and-wallets/PVC Bag.png",
+  },
+  {
+    id: 10,
+    name: "Shopping Bag",
+    image: "/products/bags-and-wallets/Shopping Bag.png",
+  },
+  {
+    id: 11,
+    name: "Totebag",
+    image: "/products/bags-and-wallets/Totebag.png",
+  },
 ];
 
 const StationeryAndOfficeSupplies = [
   {
     id: 1,
-    name: "Notebooks",
-    image:
-      "https://w7.pngwing.com/pngs/792/536/png-transparent-notebook-blue-adobe-illustrator-notebook-miscellaneous-text-rectangle.png",
+    name: "Desk Calendar",
+    image: "/products/stationery/Desk Calendar.png",
   },
   {
     id: 2,
-    name: "USB Flash Drive",
-    image:
-      "https://w7.pngwing.com/pngs/792/536/png-transparent-notebook-blue-adobe-illustrator-notebook-miscellaneous-text-rectangle.png",
+    name: "Mousepad",
+    image: "/products/stationery/Mousepad.png",
   },
   {
     id: 3,
-    name: "Planners Book",
-    image:
-      "https://w7.pngwing.com/pngs/792/536/png-transparent-notebook-blue-adobe-illustrator-notebook-miscellaneous-text-rectangle.png",
+    name: "Notebook",
+    image: "/products/stationery/Notebook.png",
   },
   {
     id: 4,
-    name: "Ballpoint Pens",
-    image:
-      "https://w7.pngwing.com/pngs/792/536/png-transparent-notebook-blue-adobe-illustrator-notebook-miscellaneous-text-rectangle.png",
+    name: "Pen",
+    image: "/products/stationery/Pen.png",
   },
   {
     id: 5,
-    name: "Mousepads",
-    image:
-      "https://w7.pngwing.com/pngs/792/536/png-transparent-notebook-blue-adobe-illustrator-notebook-miscellaneous-text-rectangle.png",
+    name: "Stationery Kit",
+    image: "/products/stationery/Stationery Kit.png",
   },
   {
     id: 6,
-    name: "Desk Calendars",
-    image:
-      "https://w7.pngwing.com/pngs/792/536/png-transparent-notebook-blue-adobe-illustrator-notebook-miscellaneous-text-rectangle.png",
+    name: "Sticky Note",
+    image: "/products/stationery/Sticky Note.png",
   },
   {
     id: 7,
-    name: "Sticky Note",
-    image:
-      "https://w7.pngwing.com/pngs/792/536/png-transparent-notebook-blue-adobe-illustrator-notebook-miscellaneous-text-rectangle.png",
-  },
-  {
-    id: 8,
-    name: "Stationery Kit",
-    image:
-      "https://w7.pngwing.com/pngs/792/536/png-transparent-notebook-blue-adobe-illustrator-notebook-miscellaneous-text-rectangle.png",
+    name: "USB Flash Drive",
+    image: "/products/stationery/USB Flash Drive.png",
   },
 ];
 
 const Electronics = [
-  { id: 11, name: "USB Flash Drive", image: "https://via.placeholder.com/150" },
-  { id: 12, name: "Headphones", image: "https://via.placeholder.com/150" },
+  {
+    id: 1,
+    name: "Bluetooth  Speakers",
+    image: "/products/electronics/Bluetooth  Speakers.png",
+  },
+  { id: 2, name: "Headphones", image: "/products/electronics/Headphone.png" },
+  {
+    id: 3,
+    name: "LED Light",
+    image: "/products/electronics/LED Light.png",
+  },
+  {
+    id: 4,
+    name: "Phone Stand",
+    image: "/products/electronics/Phone Stand.png",
+  },
+  {
+    id: 5,
+    name: "Powerbank",
+    image: "/products/electronics/Powerbank.png",
+  },
+  {
+    id: 6,
+    name: "Running Headset",
+    image: "/products/electronics/Running Headset.png",
+  },
+  {
+    id: 7,
+    name: "Smart Notebook",
+    image: "/products/electronics/Smart Notebook.png",
+  },
+  {
+    id: 8,
+    name: "TWS",
+    image: "/products/electronics/TWS.png",
+  },
+  {
+    id: 9,
+    name: "Wireless  Powerbank",
+    image: "/products/electronics/Wireless  Powerbank.png",
+  },
 ];
 
 const SafetyApparel = [
-  { id: 13, name: "Reflective Vest", image: "https://via.placeholder.com/150" },
-  { id: 14, name: "Hard Hat", image: "https://via.placeholder.com/150" },
+  { id: 1, name: "Bump Caps", image: "/products/safety-apparel/Bump Caps.png" },
+  {
+    id: 2,
+    name: "Face Shields",
+    image: "/products/safety-apparel/Face Shields.png",
+  },
+  {
+    id: 3,
+    name: "Flame-Resistant Jacket",
+    image: "/products/safety-apparel/Flame-Resistant Jacket.png",
+  },
+  {
+    id: 4,
+    name: "Full-Body Protections",
+    image: "/products/safety-apparel/Full-Body Protections.png",
+  },
+  { id: 5, name: "Helmets", image: "/products/safety-apparel/Helmets.png" },
+  {
+    id: 6,
+    name: "High-Visibility Jacket",
+    image: "/products/safety-apparel/High-Visibility Jacket.png",
+  },
+  {
+    id: 7,
+    name: "Reflective Safety Vests",
+    image: "/products/safety-apparel/Reflective Safety Vests.png",
+  },
+  {
+    id: 8,
+    name: "Respirators",
+    image: "/products/safety-apparel/Respirators.png",
+  },
+  {
+    id: 9,
+    name: "Safety Gloves",
+    image: "/products/safety-apparel/Safety Gloves.png",
+  },
+  {
+    id: 10,
+    name: "Safety Googles",
+    image: "/products/safety-apparel/Safety Googles.png",
+  },
+  {
+    id: 11,
+    name: "Safety Harnesses",
+    image: "/products/safety-apparel/Safety Harnesses.png",
+  },
+  {
+    id: 12,
+    name: "Vest (Multi Pocker)",
+    image: "/products/safety-apparel/Vest (Multi Pocker).png",
+  },
 ];
 
 const EcoFriendlyItems = [
   {
-    id: 15,
-    name: "Recycled Tote Bag",
-    image: "https://via.placeholder.com/150",
+    id: 1,
+    name: "Ashtray",
+    image: "/products/eco-friendly/Ashtray.png",
   },
-  { id: 16, name: "Bamboo Pen", image: "https://via.placeholder.com/150" },
+  {
+    id: 2,
+    name: "Bamboo Bottles",
+    image: "/products/eco-friendly/Bamboo Bottles.png",
+  },
+  {
+    id: 3,
+    name: "Bamboo Straws",
+    image: "/products/eco-friendly/Bamboo Straws.png",
+  },
+  {
+    id: 4,
+    name: "Canvas Bag",
+    image: "/products/eco-friendly/Canvas Bag.png",
+  },
+  {
+    id: 5,
+    name: "Desktop Pen Holder",
+    image: "/products/eco-friendly/Desktop Pen Holder.png",
+  },
+  {
+    id: 6,
+    name: "Jute Totebag",
+    image: "/products/eco-friendly/Jute Totebag.png",
+  },
+  {
+    id: 7,
+    name: "Keychain",
+    image: "/products/eco-friendly/Keychain.png",
+  },
+  {
+    id: 8,
+    name: "Namecard Holder",
+    image: "/products/eco-friendly/Namecard Holder.png",
+  },
+  {
+    id: 9,
+    name: "Phone Holder",
+    image: "/products/eco-friendly/Phone Holder.png",
+  },
+  {
+    id: 10,
+    name: "Recycled Pens",
+    image: "/products/eco-friendly/Recycled Pens.png",
+  },
+  {
+    id: 11,
+    name: "Stainless Straws",
+    image: "/products/eco-friendly/Stainless Straws.png",
+  },
+  {
+    id: 12,
+    name: "Trivet",
+    image: "/products/eco-friendly/Trivet.png",
+  },
 ];
 
 const DigitalPrinting = [
-  { id: 17, name: "Custom Stickers", image: "https://via.placeholder.com/150" },
-  { id: 18, name: "Brochure", image: "https://via.placeholder.com/150" },
+  {
+    id: 1,
+    name: "Flyer Pamflet",
+    image: "/products/digital-printing/Flyer_Pamflet.png",
+  },
+  {
+    id: 2,
+    name: "Packaging",
+    image: "/products/digital-printing/Packaging.png",
+  },
+  {
+    id: 3,
+    name: "Promotion Banner",
+    image: "/products/digital-printing/Promotion Banner.png",
+  },
 ];
 
 const PremiumItems = [
-  { id: 19, name: "Luxury Pen", image: "https://via.placeholder.com/150" },
+  { id: 1, name: "2XU", image: "/products/premium-brand/2XU.png" },
+  { id: 2, name: "Adidas", image: "/products/premium-brand/Adidas.png" },
+  { id: 3, name: "Diadora", image: "/products/premium-brand/Diadora.png" },
+  { id: 4, name: "DKNY", image: "/products/premium-brand/DKNY.png" },
+  { id: 5, name: "Lacoste", image: "/products/premium-brand/Lacoste.png" },
   {
-    id: 20,
-    name: "Premium Notebook",
-    image: "https://via.placeholder.com/150",
+    id: 6,
+    name: "Marks & Spencer",
+    image: "/products/premium-brand/Marks & Spencer.png",
   },
+  {
+    id: 7,
+    name: "Massino Dutti",
+    image: "/products/premium-brand/Massino Dutti.png",
+  },
+  {
+    id: 8,
+    name: "New Balance",
+    image: "/products/premium-brand/New Balance.png",
+  },
+  { id: 9, name: "New Era", image: "/products/premium-brand/New Era.png" },
+  { id: 10, name: "Nike", image: "/products/premium-brand/Nike.png" },
+  {
+    id: 11,
+    name: "On Running",
+    image: "/products/premium-brand/On Running.png",
+  },
+  {
+    id: 12,
+    name: "Onitsuka Tiger",
+    image: "/products/premium-brand/Onitsuka Tiger.png",
+  },
+  { id: 13, name: "Puma", image: "/products/premium-brand/Puma.png" },
+  { id: 14, name: "Reebok", image: "/products/premium-brand/Reebok.png" },
+  { id: 15, name: "Samsonite", image: "/products/premium-brand/Samsonite.png" },
+  { id: 16, name: "Skechers", image: "/products/premium-brand/Skechers.png" },
+  { id: 17, name: "Speedo", image: "/products/premium-brand/Speedo.png" },
+  { id: 18, name: "Srixon", image: "/products/premium-brand/Srixon.png" },
+  { id: 19, name: "Tissot", image: "/products/premium-brand/Tissot.png" },
+  { id: 20, name: "Tumi", image: "/products/premium-brand/Tumi.png" },
+  { id: 21, name: "Wilson", image: "/products/premium-brand/Wilson.png" },
 ];
 
 const Products = () => {
@@ -192,12 +537,14 @@ const Products = () => {
           className="object-cover"
         />
       </div>
-      <div className="relative w-full bg-[#FFD205]">
-        <div className="bg-[url('/small_curve.png')] h-screen w-screen bg-contain bg-no-repeat">
-          <h2 className="text-h2 text-white pr-40 pt-5 tracking-wide uppercase text-right">
-            Products
-          </h2>
-          <div className="max-w-7xl mx-auto space-x-32 space-y-4 pt-10 flex items-center justify-between">
+      <div className="relative w-full bg-[#FFD205] pb-20">
+        <div className="bg-[url('/small_curve.png')] h-full w-screen bg-contain bg-no-repeat">
+          <div className="max-w-7xl mx-auto">
+            <h2 className="text-h2 text-white pt-5 tracking-wide uppercase text-right">
+              Products
+            </h2>
+          </div>
+          <div className="max-w-7xl h-full mx-auto space-x- pt-20 flex items-start justify-between">
             <div>
               {catalogItems.map((item) => (
                 <div key={item.id}>
@@ -216,39 +563,23 @@ const Products = () => {
                 </div>
               ))}
             </div>
-            <div className="grid grid-cols-5 gap-10">
+            <div className="grid grid-cols-5 gap-5 w-max">
               {/* Baris pertama - 5 item */}
-              {selectedItems.slice(0, 5).map((item, i) => (
+              {selectedItems.map((item, i) => (
                 <div
                   key={i}
-                  className="bg-white rounded-2xl h-32 w-32 shadow-md p-4 flex flex-col items-center"
+                  className="bg-white rounded-2xl h-36 w-36 shadow-md p-4 flex flex-col items-center justify-around"
                 >
-                  <p className="text-gray-800 font-medium text-center text-sub2">
+                  <p className="text-gray-800 font-medium text-center text-sub3">
                     {item.name}
                   </p>
                   <img
-                    className="h-12 w-12 object-contain"
-                    src="https://w7.pngwing.com/pngs/792/536/png-transparent-notebook-blue-adobe-illustrator-notebook-miscellaneous-text-rectangle.png"
+                    className="h-20 w-20 object-contain"
+                    src={item.image}
                     alt={item.name}
                   />
                 </div>
               ))}
-              {/* Baris kedua - 3 item ditengah */}
-              <div className="col-span-1"></div> {/* Spacer kiri */}
-              {selectedItems.slice(5, 8).map((item, i) => (
-                <div
-                  key={i}
-                  className="bg-white rounded-2xl h-32 w-32 shadow-md p-4 flex flex-col items-center"
-                >
-                  <p className="text-gray-800 font-medium">{item.name}</p>
-                  <img
-                    className="h-12 w-12 object-contain"
-                    src="https://w7.pngwing.com/pngs/792/536/png-transparent-notebook-blue-adobe-illustrator-notebook-miscellaneous-text-rectangle.png"
-                    alt={item.name}
-                  />
-                </div>
-              ))}
-              <div className="col-span-1"></div> {/* Spacer kanan */}
             </div>
           </div>
         </div>
